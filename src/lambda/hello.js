e407721d8853afb5f537e8788783ac50d6e81753
Fawaz Farid <fawwazally@gmail.com>
2021-03-17 18:33:15 +0300

    Testing timeout log message

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,9 +1,13 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 export function handler(event, context, callback) {
-  console.log("Hello World")
-  console.log('queryStringParameters', event.queryStringParameters)
-  callback(null, {
+  console.warn("I'm a very sleepy function");
+  console.log(`The datetime is ${(new Date()).toISOString()}`);
+
+  await new Promise(resolve => setTimeout(resolve, 15000));
+
+  console.log("This probably won't get logged, because it will timeout");
+  return {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
+    body: "👋 Check the Netlify function logs",
+  };
 }
