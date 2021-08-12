357a7105cd245566fa44af9f01bb1bd0ad879d0e
Fawaz Farid <fawwazally@gmail.com>
2021-08-12 18:14:46 +0300

    Remove extra log line

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,6 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 export async function handler(event, context, callback) {
   console.warn("I'm a very lazy function");
-  console.log(`The datetime is ${(new Date()).toISOString()}`);
 
   await new Promise(resolve => setTimeout(resolve, 15000));
 
