d601c7da866711435913342350d9d77a031bcfbe
Fawaz Farid <fawwazally@gmail.com>
2021-08-12 17:33:42 +0300

    Update hello to time out

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,13 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context, callback) {
-  console.log("Hello World")
-  callback(null, {
+export async function handler(event, context, callback) {
+  console.warn("I'm a very lazy function");
+  console.log(`The datetime is ${(new Date()).toISOString()}`);
+
+  await new Promise(resolve => setTimeout(resolve, 15000));
+
+  console.log("This probably won't get logged, because it will timeout");
+  return {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
+    body: "⛔︎ Oops This timed out, check the Netlify function logs",
+  };
 }
